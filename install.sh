6d07d2f7b53eb9fb869d0db798206e8eb8956da1
Márton Münz <Marton.Muenz@icr.ac.uk>
2017-06-05 12:28:38 +0200

    --no-site-packages and --always-copy removed from virtualenv in install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ unset PYTHONPATH
 ABSOLUTE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 PIP_ARGS='--no-cache-dir --ignore-installed --force-reinstall'
 
-virtualenv -p python2.7 --no-site-packages --always-copy env
+virtualenv -p python2.7 env
 
 source ${ABSOLUTE_PATH}/env/bin/activate
 
